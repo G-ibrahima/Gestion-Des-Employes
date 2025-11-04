a06111864bc766d616dba3aeb395f0e98c208b68
Ibrahima Gueye <eb091941@ens.uqam.ca>
2025-11-04 13:00:24 -0500

    Ajout les migration de des models avec python3 manage.py makemigrations

--- Employe/employe_projet/employe/models.py
+++ Employe/employe_projet/employe/models.py
@@ -5,7 +5,7 @@ class Employe(models.Model):
     nom = models.CharField(max_length = 100)
     email = models.EmailField()
     poste = models.CharField(max_length = 100)
-    salaire = models.DateField(max_digits = 10 , decimal_places = 2)
+    salaire = models.DecimalField(max_digits = 10 , decimal_places = 2)
 
     def __str__(self):
         return self.nom
