d7a03cbbfbd159589bb37b52fda85be540a2d660
Ibrahima Gueye <eb091941@ens.uqam.ca>
2025-11-04 13:24:36 -0500

    Ajout le views.py

--- Employe/employe_projet/employe/views.py
+++ Employe/employe_projet/employe/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import *
 # Create your views here.
+
+def list_employes(request):
+    employes = Employe.objects.all()
+    return render(request,'employe/list.html', {'employes' : employes})
