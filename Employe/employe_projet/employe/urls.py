9e1f580776f3286eae65499941eaba6fe22d0186
Ibrahima Gueye <eb091941@ens.uqam.ca>
2025-11-04 15:45:36 -0500

    Ajout les html les form les urls et les views

--- Employe/employe_projet/employe/urls.py
+++ Employe/employe_projet/employe/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_employes, name = "list_employes"),
+    path("", views.list_employes, name = "list_employes"),
+    path("ajouter/", views.ajouter_employes, name = "ajouter_employes"),
+    path("modifier/<int:id>/", views.modifier_employes, name = "modifier_employes"),
+    path("supprimer/<int:id>/", views.supprimer_employes, name = "supprimer_employes"),
+
 ]
